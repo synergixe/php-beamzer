ff594374f2a9a1aba9d27f5a0550ec671ba28d30
Synergixe <10416482+synergixe@users.noreply.github.com>
2018-02-01 21:10:20 +0100

    Update function.php

--- src/function.php
+++ src/function.php
@@ -8,6 +8,16 @@ if(!function_exists('cancel_shutdown_function')){
 	}
 }
 
+if(! function_exits('normalize_laravel_notifications') ){
+  function normalize_laravel_notifications ($row){
+	$item = json_decode($row['data'], TRUE);
+	$item['nid'] = $row['id'];
+	$item['timing'] = $row['created_at'];
+	$item['is_read'] = ($row['read_at'] !== NULL);
+	  
+	return $item;
+  }
+}
 
 
 ?>
