7343313709099bcdc7b252b0a27a274da5f15b1f
Synergixe <10416482+synergixe@users.noreply.github.com>
2018-02-01 20:31:51 +0100

    Update Describable.php

--- src/Synergixe/PHPBeamzer/Describable.php
+++ src/Synergixe/PHPBeamzer/Describable.php
@@ -1,4 +1,14 @@
 <?php
 
+    namespace Synergixe\PHPBeamzer;
+
+    trait Describable {
+    
+            public function getDescription(){
+            
+                  return '';
+            }
+    
+    }
 
 ?>
