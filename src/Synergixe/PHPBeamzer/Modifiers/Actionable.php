c275b1d5a24c1d4baeb9cc9998631515d5369a92
Synergixe <10416482+synergixe@users.noreply.github.com>
2018-02-02 20:37:18 +0100

    Update Actionable.php

--- src/Synergixe/PHPBeamzer/Modifiers/Actionable.php
+++ src/Synergixe/PHPBeamzer/Modifiers/Actionable.php
@@ -1,5 +1,16 @@
 <?php
 
+/**
+ * @copyright Copyright (c) 2018 Oparand Ltd - Synergixe
+ *
+ * @version v0.1.1
+ *
+ * @author Ifeora Okechukwu (https://twitter.com/isocroft)
+ *
+ * @license MIT 
+ *
+ */
+
 namespace Synergixe\PHPBeamzer\Modifiers;
 
 trait Actionable {
