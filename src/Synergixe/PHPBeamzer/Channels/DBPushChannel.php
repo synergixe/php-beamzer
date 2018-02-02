9d0b39e9508c2092fa0b485cf5a4d0aabee3f59c
Synergixe <10416482+synergixe@users.noreply.github.com>
2018-02-02 13:55:06 +0100

    Update DBPushChannel.php

--- src/Synergixe/PHPBeamzer/Channels/DBPushChannel.php
+++ src/Synergixe/PHPBeamzer/Channels/DBPushChannel.php
@@ -1,1 +1,40 @@
+<?php
 
+/**
+ * @copyright Copyright (c) 2018 Oparand Ltd - Synergixe
+ *
+ * @version v0.1.1
+ *
+ * @author Ifeora Okechukwu (https://twitter.com/isocroft)
+ *
+ * @license MIT 
+ *
+ */
+
+namespace Synergixe\PHPBeamzer\Channels;
+
+use Illuminate\Notifications\Notification;
+
+class DBPushChannel {
+  
+	public function __construct(){
+		
+    // code..
+    
+	}	
+  
+  public function send($notifiable, Notification $notification){
+     
+	    $data = $notification->toDatabase($notifiable);
+     
+	    return $notifiable->routeNotificationFor('database')->create([
+	        'id' => $notification->id,
+	        'type' => get_class($notification),
+	        'data' => json_encode($data),  //<-- comes from toDatabase() Method above
+	        'read_at' => null,
+	        'created_at' => time(),
+	        'deleted_at' => null
+	    ]);
+  }
+}
+?>
