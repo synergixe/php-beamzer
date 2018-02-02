9cb7383135c1c22ac64bfd92c4335d860ba0e875
Synergixe <10416482+synergixe@users.noreply.github.com>
2018-02-02 20:28:38 +0100

    Update Actionable.php

--- src/Synergixe/PHPBeamzer/Modifiers/Actionable.php
+++ src/Synergixe/PHPBeamzer/Modifiers/Actionable.php
@@ -1,1 +1,33 @@
+<?php
 
+namespace Synergixe\PHPBeamzer\Modifiers;
+
+trait Actionable {
+  
+	public $no_buzz = FALSE;
+  
+	private $actions_perfomed = array();
+  
+	public function setActionPerformed($user_action = 'did', $timestamp) {
+    
+		$timestamp = strval($timestamp);
+    
+		$this->actions_perfomed[$timestamp] = $user_action;
+
+    return $this;
+	}
+  
+	public function getActionPerformed($timestamp){
+    
+		return (
+				array_key_exists($timestamp, $this->actions_perfomed)
+      
+			 ? $this->actions_perfomed[$timestamp] 
+      
+			 : NULL
+		);
+    
+	}
+}
+
+?>
