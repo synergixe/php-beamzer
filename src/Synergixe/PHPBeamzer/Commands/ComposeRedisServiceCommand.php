929c2b58c4d36348c7998fbcc47f9cf30b5d056f
Synergixe <10416482+synergixe@users.noreply.github.com>
2018-02-02 15:01:10 +0100

    Update ComposeRedisServiceCommand.php

--- src/Synergixe/PHPBeamzer/Commands/ComposeRedisServiceCommand.php
+++ src/Synergixe/PHPBeamzer/Commands/ComposeRedisServiceCommand.php
@@ -1,4 +1,46 @@
 <?php
 
+/**
+ * @copyright Copyright (c) 2018 Oparand Ltd - Synergixe
+ *
+ * @version v0.1.1
+ *
+ * @author Ifeora Okechukwu (https://twitter.com/isocroft)
+ *
+ * @license MIT 
+ *
+ */
+
+namespace Synergixe\PHPBeamzer\Commands;
+
+use Illuminate\Support\Str;
+use Illuminate\Console\Command;
+
+class ComposeRedisServiceCommand extends Command {
+
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'redis:compose';
+ 
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = "Compose A New Redis Service For Message-Driven Notification";
+ 
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function fire(){
+      
+        $this->line('Welcome to the Redis Service Composer for PHPBeamzer.');
+    }
+}
 
 ?>
