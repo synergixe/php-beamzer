248c1716f430661ac1417dd781be454f7787e25f
Synergixe <10416482+synergixe@users.noreply.github.com>
2018-02-02 15:08:38 +0100

    Update beamzer.php

--- config/beamzer.php
+++ config/beamzer.php
@@ -2,13 +2,15 @@
 
 return [
 
-	'timing' => env('BEAMZER_TIMING', '3000'),
+	'ping' => env('BEAMZER_PING_TIMEOUT', '3000'),
 	
 	'support_old_ie' => TRUE,
 	
 	'retry_limit_count' => 10,
 	
-	'' => ,
+	'use_redis_pubsub' => FALSE,
+	
+	'redis_pub_channel' => 'notifications',
 	
 	'data_chunks_size' => 5
 
