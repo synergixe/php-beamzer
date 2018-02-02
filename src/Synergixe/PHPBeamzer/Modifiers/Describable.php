eba95aa08097e57dea057f1b7978f1ae2ecc1d0a
Synergixe <10416482+synergixe@users.noreply.github.com>
2018-02-02 20:38:11 +0100

    Update Describable.php

--- src/Synergixe/PHPBeamzer/Modifiers/Describable.php
+++ src/Synergixe/PHPBeamzer/Modifiers/Describable.php
@@ -1,12 +1,23 @@
 <?php
 
+/**
+ * @copyright Copyright (c) 2018 Oparand Ltd - Synergixe
+ *
+ * @version v0.1.1
+ *
+ * @author Ifeora Okechukwu (https://twitter.com/isocroft)
+ *
+ * @license MIT 
+ *
+ */
+
   namespace Synergixe\PHPBeamzer\Modifiers;
 
   trait Describable {
     
             public function getDescription($id){
             
-                  return '';
+                  return is_null($id)? '-' : $id;
             }
     
     }
