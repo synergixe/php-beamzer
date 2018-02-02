07f9450067ad5bf92c7633f49fdd2c4a311f6540
Synergixe <10416482+synergixe@users.noreply.github.com>
2018-02-02 14:57:12 +0100

    Update BeamzerServiceProvider.php

--- src/Synergixe/PHPBeamzer/BeamzerServiceProvider.php
+++ src/Synergixe/PHPBeamzer/BeamzerServiceProvider.php
@@ -14,6 +14,7 @@
 namespace Synergixe\PHPBeamzer;
 
 use Synergixe\PHPBeamzer\Beamzer as Beamzer;
+use Synergixe\PHPBeamzer\Coomands\ComposeRedisServiceCommand as ComposeRedisServiceCommand;
 use Illuminate\Support\ServiceProvider;
 
 class BeamzerServiceProvider extends ServiceProvider {
@@ -34,14 +35,15 @@ class BeamzerServiceProvider extends ServiceProvider {
 	 */
 
 	public function boot(Beamzer $beam){
+		
+		$this->loadConfig();
+		// $this->loadMigrations();
 
 		if($this->app->runningInConsole()){
-			$this->loadConfig();
-			// $this->loadMigrations();
-
-			/*$this->commands([
-
-			]);*/
+			
+			$this->commands([
+				ComposeRedisServiceCommand::class
+			]);
 		}
 	}
 
@@ -54,20 +56,26 @@ class BeamzerServiceProvider extends ServiceProvider {
 
 	public function register(){
 
-		$this->mergeConfigFrom(__DIR__.'/../../config/beamzer.php', 'beamzer');
+		// $this->mergeConfigFrom(__DIR__.'/../../config/beamzer.php', 'beamzer');
 		
 		$this->app->singleton('Streamer', function($app){
 			
 			/*
-				if($app->request->hasHeader('Last-Event-ID')){
-				    $last_event_id = $app->request->header('Last-Event-ID', 0);
-				}
+				Setup the Last-Event-Id value for the package
 			*/
 		
 			$last_event_id = $app->request->query->('lastEventId');
 			
-			if(!isset($last_event_id)){
+			/*if(!isset($last_event_id)){
 				 $last_event_id = $app->request->headers->get('LAST_EVENT_ID');
+			}*/
+			
+			if(!isset($last_event_id) 
+			   	&& $app->request->hasHeader('Last-Event-ID')){
+				    $last_event_id = $app->request->header('Last-Event-ID', 0);
+			}else{
+			
+				$last_event_id = NULL;
 			}
 			
 			$app->request->query->add(['lastEventId' => $last_event_id]);
