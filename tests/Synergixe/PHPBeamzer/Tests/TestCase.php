4dfc55ec41f830e4aaa5361cf628be299d499db2
SynergixeNG <10416482+synergixe@users.noreply.github.com>
2018-02-08 18:21:32 +0100

    Update TestCase.php

--- tests/Synergixe/PHPBeamzer/Tests/TestCase.php
+++ tests/Synergixe/PHPBeamzer/Tests/TestCase.php
@@ -1,1 +1,47 @@
+<?php
 
+namespace Synergixe\PHPBeamzer\Test;
+
+use Synergixe\PHPBeamzer\Facades\Laravel\Streamer;
+use Synergixe\PHPBeamzer\Providers\Laravel\BeamzerServiceProvider;
+use Orchestra\Testbench\TestCase as OrchestraTestCase;
+
+class TestCase extends OrchestraTestCase{
+  
+    /**
+     * Load package service provider
+     * @param  \Illuminate\Foundation\Application $app
+     *
+     * @return Synergixe\PHPBeamzer\Providers\Laravel\BeamzerServiceProvider
+     */
+  
+    protected function getPackageProviders($app){
+      
+        return [BeamzerServiceProvider::class];
+      
+    }
+    /**
+     * Load package alias
+     * @param  \Illuminate\Foundation\Application $app
+     * @return array
+     */
+    protected function getPackageAliases($app){
+      
+        return [
+            'Streamer' => Streamer::class,
+        ];
+      
+    }
+    
+    /*public function mock($class){
+        
+        $mock = Mockery::mock($class);
+        
+        $this->app->instance($class, $mock);
+        
+        return $mock;
+        
+    }*/
+}
+
+?>
