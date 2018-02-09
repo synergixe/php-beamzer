7202e4a720df2b0d89702d0520d069b8d491609b
SynergixeNG <10416482+synergixe@users.noreply.github.com>
2018-02-09 20:21:14 +0100

    Update function.php

--- src/function.php
+++ src/function.php
@@ -3,14 +3,17 @@
 
 if(! function_exists('cancel_shutdown_function') ){
 	function cancel_shutdown_function($envelope){
-    		
-    		$envelope->cancel();
+    		if(method_exists($envelope, 'cancel')){
+    			$envelope->cancel();
+		}
 	}
 }
 
 if(! function_exists('normalize_laravel_notifications') ){
-  function normalize_laravel_notifications ($row){
-	$item = json_decode($row['data'], TRUE);
+  function normalize_laravel_notifications ($_row){
+	$row = is_array($_row) ? $_row : array('data'=>'[]','id'=>'','created_at'=>'','read_at'=>NULL);
+	$item = @json_decode($row['data'], TRUE);
+	  
 	$item['nid'] = $row['id'];
 	$item['timing'] = $row['created_at'];
 	$item['is_read'] = !(is_null($row['read_at']));
