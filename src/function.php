77d6074660a3aca28abf8bdb63df42d34b8336ec
SynergixeNG <10416482+synergixe@users.noreply.github.com>
2018-02-13 15:11:32 +0100

    Update function.php

--- src/function.php
+++ src/function.php
@@ -12,7 +12,8 @@ if(! function_exists('cancel_shutdown_function') ){
 if(! function_exists('normalize_laravel_notifications') ){
   function normalize_laravel_notifications ($_row){
 	$row = is_array($_row) ? $_row : array('data'=>'[]','id'=>'','created_at'=>'','read_at'=>NULL);
-	$item = @json_decode($row['data'], TRUE);
+	  $item = array();
+	$item['payload'] = @json_decode($row['data'], TRUE);
 	  
 	$item['nid'] = $row['id'];
 	$item['timing'] = $row['created_at'];
