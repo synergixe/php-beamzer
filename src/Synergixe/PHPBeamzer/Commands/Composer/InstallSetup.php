d07b07a39c0289ada9b3c65d389fb9d74011d866
SynergixeNG <10416482+synergixe@users.noreply.github.com>
2018-03-29 12:26:07 +0100

    Update InstallSetup.php

--- src/Synergixe/PHPBeamzer/Commands/Composer/InstallSetup.php
+++ src/Synergixe/PHPBeamzer/Commands/Composer/InstallSetup.php
@@ -6,6 +6,18 @@ use Composer\Script\Event;
 use Composer\Installer\PackageEvent;
 
 class InstallSetup {
+    
+    public static function preInstall(Event $event) {
+        // provides access to the current ComposerIOConsoleIO
+        // stream for terminal input/output
+        $io = $event->getIO();
+        if ($io->askConfirmation("Are you sure you want to install php-beamzer ? ", false)) {
+            // ok, continue on to composer install
+            return true;
+        }
+        // exit composer and terminate installation process
+        exit;
+    }
 
     public static function postUpdate(Event $event){
     
@@ -21,14 +33,17 @@ class InstallSetup {
 
     public static function postPackageInstall(PackageEvent $event){
     
-        $installedPackage = $event->getOperation()->getPackage();
+        $installedPackage = $event->getComposer()->getPackage();
         // do stuff
     }
 
     public static function postInstall(Event $event){
-    
+        // provides access to the current Composer instance
+        $composer = $event->getComposer();
         // do stuff
     }
+    
+    // Synergixe\PHPBeamzer\Commands\Composer\InstallSetup::postPackageInstall
 }
 
 ?>
