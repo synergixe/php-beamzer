47fb85efa7e3e95b1e571766c3c7b66a3934f2b8
SynergixeNG <10416482+synergixe@users.noreply.github.com>
2018-05-14 18:29:56 +0100

    Create SynNotification.php

---
+++ src/Synergixe/PHPBeamzer/SynNotification.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @copyright Copyright (c) 2018 Oparand Ltd - Synergixe
+ *
+ * @version v0.1.3
+ *
+ * @author Ifeora Okechukwu (https://twitter.com/isocroft)
+ *
+ * @license MIT 
+ *
+ */
+ 
+ namespace Synergixe\PHPBeamzer;
+
+use Illuminate\Notifications\DatabaseNotification;
+
+class SynNotification extends DatabaseNotification {
+    
+    public function morphsToMany(){
+      
+      ;
+    }
+}
