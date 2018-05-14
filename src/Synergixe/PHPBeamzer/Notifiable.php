022ad91edf75d1053f2a5120db140e4a871d1128
SynergixeNG <10416482+synergixe@users.noreply.github.com>
2018-05-14 18:27:19 +0100

    Update Notifiable.php

--- src/Synergixe/PHPBeamzer/Notifiable.php
+++ src/Synergixe/PHPBeamzer/Notifiable.php
@@ -1,3 +1,16 @@
+<?php
+
+/**
+ * @copyright Copyright (c) 2018 Oparand Ltd - Synergixe
+ *
+ * @version v0.1.3
+ *
+ * @author Ifeora Okechukwu (https://twitter.com/isocroft)
+ *
+ * @license MIT 
+ *
+ */
+
 namespace Synergixe\PHPBeamzer;
 
 use Illuminate\Notifications\Notifiable as BaseNotifiable;
