dae3153bad4ed4aeab14bdb8bb14c0288d95b703
SynergixeNG <10416482+synergixe@users.noreply.github.com>
2018-05-24 16:08:26 +0100

    Update function.php

--- src/function.php
+++ src/function.php
@@ -1,6 +1,26 @@
 
 <?php
 
+if(! function_exists('is_actionable')){
+
+	function is_actionable($class_name, $key){
+		
+		$short_name = (substr($class_name, strrpos($class_name, '\\') + 1));
+		
+		return ($short_name === 'Actionable');
+	}
+}
+
+if(! function_exists('is_describable')){
+
+	function is_describable($class_name, $key){
+		
+		$short_name = (substr($class_name, strrpos($class_name, '\\') + 1));
+		
+		return ($short_name === 'Describable');
+	}
+}
+
 if(! function_exists('cancel_shutdown_function') ){
 	function cancel_shutdown_function($envelope){
     		if(method_exists($envelope, 'cancel')){
@@ -11,9 +31,12 @@ if(! function_exists('cancel_shutdown_function') ){
 
 if(! function_exists('normalize_laravel_notifications') ){
   function normalize_laravel_notifications ($_row){
-	$row = is_array($_row) ? $_row : array('data'=>'[]','id'=>'','created_at'=>'','read_at'=>NULL);
+	  
+	 $row = is_array($_row) ? $_row : array('data'=>'[]','id'=>'','created_at'=>'','read_at'=>NULL);
+	  
 	  $item = array();
-	$item['payload'] = @json_decode($row['data'], TRUE);
+	
+	  $item['payload'] = @json_decode($row['data'], TRUE);
 	  
 	$item['nid'] = $row['id'];
 	$item['timing'] = $row['created_at'];
