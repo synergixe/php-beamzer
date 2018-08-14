5b838d92a1b23e2364b0927ef0f0b039aa508271
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2018-08-14 12:39:11 +0100

    Update BufferBustingEchoHandler.php

--- src/Synergixe/PHPBeamzer/Helpers/BufferBustingEchoHandler.php
+++ src/Synergixe/PHPBeamzer/Helpers/BufferBustingEchoHandler.php
@@ -1,5 +1,16 @@
 <?php
 
+/**
+ * @copyright Copyright (c) 2018 Oparand Ltd - Synergixe
+ *
+ * @version v0.1.7
+ *
+ * @author Ifeora Okechukwu (https://twitter.com/isocroft)
+ *
+ * @license MIT
+ *
+ */
+
 namespace Synergixe\PHPBeamzer\Helpers;
 
 use Igorw\EventSource\EchoHandler;
