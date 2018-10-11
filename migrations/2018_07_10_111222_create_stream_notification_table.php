d8af0689bdeef6396a2e4b59a06ac0c4bd2297e1
SynergixeNG <10416482+synergixe@users.noreply.github.com>
2018-10-11 17:38:33 +0100

    Create 2018_07_10_111222_create_stream_notification_table.php

---
+++ migrations/2018_07_10_111222_create_stream_notification_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+ 
+class CreateStreamNotificaitionTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(){
+    
+        Schema::create('stream_notifications', function (Blueprint $table){
+            $table->('id');
+            $table->integer('user_id')->unsigned()->index();
+            $table->string('type', 220);
+            $table->datetime('read_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(){
+    
+        Schema::drop('synnotifications');
+    }
+}
